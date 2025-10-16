3fd95faf6a016d18906f4845352c7eabaacd0f0c
Flavio Di Natale <dinataleflavio@gmail.com>
2025-10-16 17:52:32 +0200

    implement short and long press detection

--- src/main.py
+++ src/main.py
@@ -1,26 +1,50 @@
-from machine import Pin
-from usr_button import UsrButton
+from usr_button import UsrButton, LONG_PRESSED, SHORT_PRESSED
 from rgb_led import RGBled
-from colors import ColorIterator, RGB
+from colors import OFF, GREEN, BLUE
 
 RGB_PIN = 23
 BTN_PIN = 24
 LED_PIN = 25
 
-# Initialize blue LED
-blue_led = Pin(LED_PIN, Pin.OUT)
+WAITING = "waiting"
+SELECTED = "selected"
+RUNNING = "running"
+DONE = "done"
 
 
 def loop():
+    rgb = RGBled(RGB_PIN)
+    rgb.set_color(OFF)
+
     button = UsrButton(BTN_PIN)
-    rgb_led = RGBled(RGB_PIN)
-    color_iterator = ColorIterator(RGB)
+    state = WAITING
+
     while True:
-        if button.released():
-            text, numerical = next(color_iterator)
-            rgb_led.set_color(numerical)
-            blue_led.toggle()
-            print(text)
+        check_event(button, rgb)
+        do(state)
+
+
+def do(state):
+    if state == WAITING:
+        do_blink()
+    elif state == SELECTED:
+        do_selected()
+
+
+def check_event(button, rgb):
+    detected_event = button.run()
+    if detected_event == LONG_PRESSED:
+        rgb.set_color(GREEN)
+    elif detected_event == SHORT_PRESSED:
+        rgb.set_color(BLUE)
+
+
+def do_blink():
+    pass
+
+
+def do_selected():
+    pass
 
 
 if __name__ == "__main__":
--- src/usr_button.py
+++ src/usr_button.py
@@ -1,16 +1,47 @@
 from machine import Pin
+from time import ticks_ms, ticks_add, sleep_ms
 
 PRESSED = 0
 RELEASED = 1
+
+SHORT_PRESSED = "short"
+LONG_PRESSED = "long"
+
+DURATION_TRESHOLD = 300
+
+TICKS_MAX = ticks_add(0, -1)
 
 
 class UsrButton:
     def __init__(self, gpio):
         self.pin = Pin(gpio, Pin.IN, Pin.PULL_UP)
+        # reading immediatelly cause issue, wait a little
+        sleep_ms(100)
         self.previous_status = self.pin.value()
 
-    def released(self):
+    def _detect_transition(self):
         current_status = self.pin.value()
-        pressed = self.previous_status == PRESSED and current_status == RELEASED
+        detected = None
+        if self.previous_status != current_status:
+            if current_status == RELEASED:
+                current_tick = ticks_ms()
+                duration = self._press_duration(current_tick)
+                print(f"the duration was {TICKS_MAX} - {duration}")
+                if duration < DURATION_TRESHOLD:
+                    detected = SHORT_PRESSED
+                else:
+                    detected = LONG_PRESSED
+            else:
+                self.pressed_at = ticks_ms()
         self.previous_status = current_status
-        return pressed
+        return detected
+
+    def _press_duration(self, end_tick):
+        if self.pressed_at <= end_tick:
+            return end_tick - self.pressed_at
+        else:
+            return TICKS_MAX - self.pressed_at + end_tick
+
+    def run(self):
+        transition = self._detect_transition()
+        return transition
